1a75a6d24aa1e9d1ba1a60ee19d8538af3a19832
Vallem Balu <vbnreddy5@gmail.com>
2021-08-13 19:14:35 +0530

    Data Base Connection  to console app

---
+++ AddressBookUsing_ADO.NET/AddressBookRepo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookUsing_ADO.NET
+{
+    public class AddressBookRepo
+    {
+        public static string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=addressbook_service;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        SqlConnection connection = new SqlConnection(connectionString);
+
+        public void CheckConnection()
+        {
+            try
+            {
+                using (this.connection)
+                {
+                    connection.Open();
+                    Console.WriteLine("Database_Connected_Successfully....");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Database_NOT_Connected!!!");
+            }
+            finally
+            {
+                this.connection.Close();
+            }
+        }
+    }
+}
---
+++ AddressBookUsing_ADO.NET/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AddressBookUsing_ADO.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AddressBook_Using_Ado.Net ");
+            AddressBookRepo abrepo = new AddressBookRepo();
+            abrepo.CheckConnection();
+            Console.ReadKey();
+        }
+    }
+}
