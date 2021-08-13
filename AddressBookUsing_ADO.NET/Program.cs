f0c69a35ed04e9eaa74b5bf1dcbb07c9664e7e29
Vallem Balu <vbnreddy5@gmail.com>
2021-08-13 22:25:43 +0530

    add new contacts in addressbook

--- AddressBookUsing_ADO.NET/Program.cs
+++ AddressBookUsing_ADO.NET/Program.cs
@@ -8,7 +8,25 @@ namespace AddressBookUsing_ADO.NET
         {
             Console.WriteLine("AddressBook_Using_Ado.Net ");
             AddressBookRepo abrepo = new AddressBookRepo();
-            abrepo.CheckConnection();
+            //abrepo.CheckConnection();
+
+            AddressBookModel abmodel = new AddressBookModel();
+            abmodel.First_Name = "Balanagireddy";
+            abmodel.Last_Name = "Vallem";
+            abmodel.Address = "kdkr";
+            abmodel.City = "Ongl";
+            abmodel.State = "andhrapradesh";
+            abmodel.Zip = "523117";
+            abmodel.Phone_Number = "9492407486";
+            abmodel.Email = "vbnreddys@gmail.com";
+            abmodel.BookName = "address002";
+            abmodel.AddressbookType = "family";
+            bool result = abrepo.AddContact(abmodel);
+
+            if (result)
+                Console.WriteLine("Record added successfully...");
+            else
+                Console.WriteLine("Some problem is there...");
             Console.ReadKey();
         }
     }
---
+++ Addressbookstore.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE addressProcedure
+@first_name varchar(20) ,
+@last_name varchar(20) ,
+@address varchar(20),
+@city varchar(20),
+@state varchar(13),
+@zip varchar(6),
+@phone_number varchar(10),
+@email varchar(20),
+@bookname varchar(20) ,
+@addressbooktype varchar(20)
+as
+insert into address_book values(@first_name,@last_name,@address,@city,@state,@zip,@phone_number,
+    @email,@bookname,@addressbooktype);
+go
+
+select * from address_book
